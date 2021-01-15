68a011709a36758bf341e0b57a033b7437b8c86c
dkuchan415 <danielkuchan@yahoo.com>
2021-01-14 23:12:05 -0600

    Final commit for step 1

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 
 //Create variable for map
 
-function createMap(earthquakes) {
+function createMap(myMarkerLayer) {
 
 var myMap = L.map("mapid", {
     center: [39.82, -98.58],
@@ -19,6 +19,47 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+var legend = L.control({position: 'bottomright'});
+
+//Add legend- from Leaflet documentation
+
+function getColor(d) {
+    return d > 90  ? '#d73027' :
+           d > 70  ? '#fc8d59' :
+           d > 50   ? '#fee08b' :
+           d > 30   ? '#d9ef8b' :
+           d > 10   ? '#91cf60' :
+                      '#1a9850';
+}
+
+//Create legend- from leaflet documentation
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        depths = [-10, 10, 30, 50, 70, 90],
+        labels = ["-10-10", "10-30", "30-50", "50-70", "70-90", "90+"];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < depths.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
+            depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
+
+// var legend = L.control({position: 'bottomright'});
+
+myMarkerLayer.addTo(myMap);
+// legend.addTo(myMap)
+console.log(myMarkerLayer);
+
 //Add layer control to the map
 
 // L.control.layers(baseMaps, overlayMaps, {
@@ -32,25 +73,37 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson"
 
+
 //Grab the data with d3
 
 function createMarkers(response) {
+    function getColor(d) {
+        return d > 90  ? '#d73027' :
+               d > 70  ? '#fc8d59' :
+               d > 50   ? '#fee08b' :
+               d > 30   ? '#d9ef8b' :
+               d > 10   ? '#91cf60' :
+                          '#1a9850';
+    }
     console.log(response);
-    var earthquakes = L.geoJSON(response.features);
-    console.log(earthquakes);
+    var earthquakes = response.features;
 
     var markers = [];
 
     for (var i = 0; i < response.features.length; i++) {
         //Set the earthquake location property to a variable
-        var earthquake = L.geoJSON(earthquakes[i]);
+        var earthquake = earthquakes[i];
         console.log(earthquake);
         if (earthquake) {
-            var marker = L.circleMarker([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[1]])
-            .bindPopup("<h3>Location:" + earthquake.properties.place);
-            markers.push(marker);
-            console.log(markers);
+            var marker = L.circle([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]], {
+                fillColor: getColor(earthquake.geometry.coordinates[2]),
+                color: getColor(earthquake.geometry.coordinates[2]),
+                fillOpacity: 0.75,
+                radius: (earthquake.properties.mag*50000)
+            })
             //Add magnitude as Size: earthquake.properties.mag
+            .bindPopup("<h3>Location:" + earthquake.properties.place +"<h4>Magnitude: " + earthquake.properties.mag + "<h4>Depth: " + earthquake.geometry.coordinates[2]);
+            markers.push(marker);
             //Add depth of earthquake as color: earthquake.geometry.coordinates[2]
         }
     }
